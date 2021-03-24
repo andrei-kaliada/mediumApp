e7b4994c65676932f4de2fdaccf0033459907851
andrei-kaliada <koleda05@gmail.com>
2021-03-24 23:45:59 +0100

    Create Login page

--- medium.app_vue/src/views/Register.vue
+++ medium.app_vue/src/views/Register.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="auth-page">
-        <div class="conteinr page">
+        <div class="container page">
             <div class="row">
                 <div class="col-md-6 offset-md-3 col-xs-12">
                     <h1 class="text-xs-center">Sign Up</h1>
@@ -8,25 +8,33 @@
                         <router-link :to="{name: 'login'}">Need an account?</router-link>
                     </p>
                     VALIDATION ERRORS
-                    <form @sumbit.prevent="onSubmit">
+                    <form @submit.prevent="onSubmit">
                         <fieldset class="form-group">
                             <input 
                             type="text" 
-                            class="form-contol form-control-lg" 
+                            class="form-control form-control-lg" 
                             placeholder="Username" 
                             />
-                            <input 
+                         </fieldset>
+                         <fieldset class="form-group">
+                             <input 
                             type="text" 
-                            class="form-contol form-control-lg" 
+                            class="form-control form-control-lg" 
                             placeholder="Email"
                             />
+                         </fieldset>
+                        <fieldset class="form-group">
                             <input 
-                            type="text" 
-                            class="form-contol form-control-lg" 
+                            type="password" 
+                            class="form-control form-control-lg" 
                             placeholder="Password" 
                             />
-                            <button class="btn btn-lg btn-primary pull-xs-right">Sign Up</button>
                         </fieldset>
+                        <button 
+                        :disabled = "isSubmitting"
+                         class="btn btn-lg btn-primary pull-xs-right">
+                            Sign Up
+                        </button>
                     </form>
                 </div>
             </div>
@@ -37,6 +45,17 @@
 
 <script>
 export default {
-    name:'McvRegister'
+    name:'McvRegister',
+    computed:{
+        isSubmitting(){
+            return this.$store.state.auth.isSubmitting
+        }
+    },
+    methods:{
+        onSubmit(){
+            console.log('Submit')
+            this.$store.commit('registerStart');
+        }
+    }
 }
 </script>
